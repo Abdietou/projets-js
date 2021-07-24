f96aa1a000f68fcced887a4e7dac2d385da9dc7d
Abdietou <a.doucoure@ymail.com>
2021-07-24 19:28:45 +0200

    autre type de test

--- jest-js-2/tests/timer.test.js
+++ jest-js-2/tests/timer.test.js
@@ -2,9 +2,15 @@ import { sleep } from "../timer.js"
 
 describe('Timer', () => {
 
-    it('shoud wait 3 seconds', async () => {
+    it.concurrent('shoud wait 3 seconds', async () => {
         const t = Date.now();
         await sleep(3);
         expect(Date.now() - t).toBeGreaterThanOrEqual(3000);
+    });
+
+    it.concurrent('shoud wait 3 seconds', async () => {
+        const t = Date.now();
+        await sleep(2);
+        expect(Date.now() - t).toBeGreaterThanOrEqual(2000);
     });
 });
