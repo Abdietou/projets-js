23cd735ff273f282bdb437a7ee8da6bb3e3e47e0
Abdietou <a.doucoure@ymail.com>
2021-08-14 16:08:12 +0200

    routing home and add techno pages

--- angular10-app/src/app/app.module.ts
+++ angular10-app/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TechnoAddComponent } from './techno-add/techno-add.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, TechnoAddComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
