6475bce69d299391b62d7ecd10bc07f181504f8e
Doucouré Abdietou <a.doucoure@ymail.com>
2021-08-18 23:49:47 +0200

    create store vuex and action, mutation etc

--- shop-vue3/src/store/index.js
+++ shop-vue3/src/store/index.js
@@ -1,15 +1,21 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
-    
+    products: [],
   },
   mutations: {
-    
+    loadProduct(state, products) {
+      state.products = products;
+    },
   },
   actions: {
-
+    loadProduct({ commit }) {
+      axios
+        .get("https://fakestoreapi.com/products")
+        .then((res) => commit("loadProduct", res.data));
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
