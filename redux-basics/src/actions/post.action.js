e0c9f27f9df51cb97d66ece8fdd71c0622b7df6f
Doucouré Abdietou <a.doucoure@ymail.com>
2022-02-04 00:49:22 +0100

    post data with redux

--- redux-basics/src/actions/post.action.js
+++ redux-basics/src/actions/post.action.js
@@ -1,16 +1,31 @@
 import axios from "axios";
 
 export const GET_POST = "GET_POST";
+export const ADD_POST = "ADD_POST";
 
 const urlApi = "http://localhost:3000";
-const postApiByOrder = "/posts?_sort=id&_order=desc";
+const getPostApiByOrder = "/posts?_sort=id&_order=desc";
+const addPostApi = "/posts";
 
 export const getPosts = () => {
   return (dispatch) => {
     return axios
-      .get(urlApi + postApiByOrder)
+      .get(urlApi + getPostApiByOrder)
       .then((response) => {
         dispatch({ type: GET_POST, payload: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
+
+export const addPosts = (data) => {
+  return (dispatch) => {
+    return axios
+      .post(urlApi + addPostApi, data)
+      .then((response) => {
+        dispatch({ type: ADD_POST, payload: data });
       })
       .catch((error) => {
         console.log(error);
--- redux-basics/src/reducers/post.reducer.js
+++ redux-basics/src/reducers/post.reducer.js
@@ -1,10 +1,12 @@
-import { GET_POST } from "../actions/post.action";
+import { ADD_POST, GET_POST } from "../actions/post.action";
 
 const initialState = {};
 
 export default function postReducer(state = initialState, action) {
   if (action.type === GET_POST) {
     return action.payload;
+  } else if (action.type === ADD_POST) {
+    return [action.payload, ...state];
   } else {
     return state;
   }
