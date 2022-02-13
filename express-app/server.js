6911383b2e132ba5a3dbcbc147da65ad507d39a5
Doucouré Abdietou <a.doucoure@ymail.com>
2022-02-13 21:48:55 +0100

    Template twig and middlewares

--- express-app/server.js
+++ express-app/server.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const port = 3013;
 
+app.set("view engine", "twig");
+
+app.use(express.static("public"));
+
+app.use((request, response, next) => {
+  request.message = "message middleware";
+  next();
+});
+
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/index.html");
+  //response.sendFile(__dirname + "/index.html");
+  response.render("index", { title: "home page" });
 });
 
 app.get("/contact", (request, response) => {
-  response.sendFile(__dirname + "/contact.html");
+  //console.log(request.message);
+  //response.sendFile(__dirname + "/contact.html");
+  response.render("contact");
 });
 
 app.listen(port, () => {
