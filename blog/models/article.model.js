99c0f19f7400df4ca7711b1a44986e10b5a4e201
Doucouré Abdietou <a.doucoure@ymail.com>
2022-02-14 23:02:19 +0100

    Revert "init and use mongoDB"
    This reverts commit 73b7566b9a11cd42ea948c5d204122c29802e8d9.

--- blog/models/article.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const articleSchema = mongoose.Schema({
-  name: String,
-  content: String,
-  publishedDate: Date,
-});
-
-module.exports = mongoose.model("Article", articleSchema);
